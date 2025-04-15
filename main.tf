1701fe4dabe41f0888f6b4f38123e5a515ab57b3
TIANDA-ZHANG <tianda.zhang@ibm.com>
2025-04-15 16:38:48 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform { 
+  cloud { 
+    
+    organization = "tianda" 
+
+    workspaces { 
+      name = "module-test" 
+    } 
+  } 
+}
+
+module "pet" {
+  source  = "app.terraform.io/tianda/pet/random"
+  version = "0.0.1"
+  pet_length = 1
+  pet_prefix = "module"
+}
